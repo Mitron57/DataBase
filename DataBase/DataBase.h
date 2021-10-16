73e393fe331b846d625664d50db0c0550aac4dfd
Mitron57 <85793630+Mitron57@users.noreply.github.com>
2021-10-16 17:00:14 +0400

    Delete DataBase directory

--- DataBase/DataBase.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <fstream>
-#include <iostream>
-
-#include "Definition.h"
-#include "UniBase.h"
-#include "Writer.h"
--- DataBase/DataBase/Definition.h
+++
@@ -1,5 +0,0 @@
-namespace Data::UniBase
-{
-    std::fstream doc;
-    std::string path{};
-}
