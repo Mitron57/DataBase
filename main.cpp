243f3c0999288a84836a5acd16b26c30d1f295a1
Mitron57 <85793630+Mitron57@users.noreply.github.com>
2022-04-01 23:16:42 +0400

    use switch instead of if/else construction

--- main.cpp
+++ main.cpp
@@ -6,33 +6,34 @@ auto parse_args(char** begin, char** end) {
     char** arg = std::find(begin, end, std::string("-preset"));
     if(arg != end && ++arg!=end) {
         char argument = *(*arg);
-        if(argument=='1') {
-            Writer<>::Write();
-            Collector::Collect();
-            std::cout<<"Do you want to read list?[y\\n]\n";
-            char answer = ' ';
-            std::cin>>answer;
-            if(answer=='y') { Reader::Read(); }
-            else { return; }
-        }
-        else if(argument=='2') {
-            Editor::Edit();
-            std::cout<<"Do you want to read list?[y\\n]\n";
-            char answer = ' ';
-            std::cin>>answer;
-            if(answer=='y') { Reader::Read(); }
-            else { return; }
-        }
-        else if(argument=='3') {
-            Reader::Read();
-        }
-        else if(argument=='4'){
-            Reader::Read();
-            Writer<>::Write();
-            Collector::Collect();
-            Editor::Edit();
-            Reader::Read();
-        }
+        switch(argument){
+            case '1': {
+                Writer<>::Write();
+                Collector::Collect();
+                std::cout << "Do you want to read list?[y\\n]\n";
+                char answer = ' ';
+                std::cin >> answer;
+                if (answer == 'y') { Reader::Read(); }
+                else { return; }
+            }
+            case '2': {
+                Editor::Edit();
+                std::cout<<"Do you want to read list?[y\\n]\n";
+                char answer = ' ';
+                std::cin>>answer;
+                if(answer=='y') { Reader::Read(); }
+                else { return; }
+            }
+            case '3':
+                Reader::Read();
+            case '4': {
+                Reader::Read();
+                Writer<>::Write();
+                Collector::Collect();
+                Editor::Edit();
+                Reader::Read();
+            }   
+        };
     } 
     else { throw std::runtime_error("Not enough arguments"); }
 }
